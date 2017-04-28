5a65dba25fee4f3512328d1ade927ca083b0cefc
arno-st <arno@daika08.ch>
2017-04-28 12:29:11 +0200

    Add files via upload

---
+++ map.php
@@ -0,0 +1,171 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2007 The Cacti Group                                      |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+$guest_account = true;
+chdir('../../');
+include("./include/auth.php");
+
+map_setup_table();
+map_check_upgrade();
+
+/* if the user pushed the 'clear' button */
+if (isset($_REQUEST["button_clear_x"])) {
+	kill_session_var("sess_map_host");
+
+	unset($_REQUEST["hostname"]);
+}
+
+/* remember these search fields in session vars so we don't have to keep passing them around */
+load_current_session_value("hostname", "sess_map_host", "");
+
+$sql_where  = '';
+$hostname       = get_request_var_request("hostname");
+
+if ($hostname != '') {
+	$sql_where .= " AND " . "host.hostname like '%$hostname%'";
+}
+
+include(dirname(__FILE__) . "/general_header.php");
+
+$total_rows = db_fetch_cell("SELECT
+	COUNT(host.id)
+	FROM host, plugin_map_coordinate map
+	WHERE host.id=map.host_id
+	$sql_where");
+
+$sql_query = "SELECT host.id as 'id', 
+		host.hostname as 'hostname', map.lat as 'lat', map.lon as 'lon', map.address as 'address' 
+		FROM host, plugin_map_coordinate map
+		WHERE host.id=map.host_id
+		$sql_where 
+		ORDER BY host.id";
+
+$result = db_fetch_assoc($sql_query);
+?>
+<script type="text/javascript">
+<!--
+
+function applyFilterChange(objForm) {
+	strURL = '&hostname=' + objForm.host.value;
+	document.location = strURL;
+}
+
+-->
+</script>
+
+<?php
+// TOP DEVICE SELECTION
+html_start_box("<strong>Filters</strong>", "100%", $colors["header"], "3", "center", "");
+
+?>
+<tr bgcolor="#<?php print $colors["panel"];?>" class="noprint">
+	<td class="noprint">
+	<form style="padding:0px;margin:0px;" name="form" method="get" action="<?php print $config['url_path'];?>plugins/map/map.php">
+		<table width="100%" cellpadding="0" cellspacing="0">
+			<tr class="noprint">
+				<td nowrap style='white-space: nowrap;' width="1">
+					&nbsp;Hostname :&nbsp;
+				</td>
+				<td width="1">
+					<input type="text" name="hostname" size="25" value="<?php print get_request_var_request("hostname");?>">
+				</td>
+				<td nowrap style='white-space: nowrap;'>
+					&nbsp;<input type="submit" value="Go" title="Set/Refresh Filters">
+					<input type="submit" name="button_clear_x" value="Clear" title="Reset fields to defaults">
+				</td>
+			</tr>
+		</table>
+	</form>
+	</td>
+</tr>
+<?php
+html_end_box();
+
+html_start_box("", "100%", $colors["header"], "3", "center", "");
+?>
+
+    <style>
+      #map-container {
+        padding: 6px;
+        border-width: 1px;
+        border-style: solid;
+        border-color: #ccc #ccc #999 #ccc;
+        -webkit-box-shadow: rgba(64, 64, 64, 0.5) 0 2px 5px;
+        -moz-box-shadow: rgba(64, 64, 64, 0.5) 0 2px 5px;
+        box-shadow: rgba(64, 64, 64, 0.1) 0 2px 5px;
+        width: 800px;
+      }
+      #map {
+        width: 800px;
+        height: 500px;
+      }
+    </style>
+
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+//    <script src="http://lslcact01.lausanne.ch/cacti/plugins/map/markerclusterer.js"></script>
+
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAr0rad39hJtQLiRoPqsTstFW9u8kl6PYA&callback=initMap"></script>
+	<script>
+	function initMap() {
+        var center = new google.maps.LatLng(46.52, 6.64);
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: center,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+		
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+		var markers = [];
+		
+
+<?php
+		foreach( $result as $device ) {
+		// get latitude, longitude and formatted address
+?>
+			var marker = new google.maps.Marker( {
+				position: new google.maps.LatLng(<?php print $device['lat'];?>, <?php print $device['lon'];?>),
+				title: '<?php print $device['address'];?>'
+			} );
+			markers.push(marker);
+<?php
+		}
+?>
+		var markerCluster = new MarkerClusterer(map, markers, {imagePath: './images/m'});
+    }
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+</script>
+  <body>
+    <div id="map-container"><div id="map"></div></div>
+  </body>
+
+<?php
+
+html_end_box(false);
+
+include_once("./include/bottom_footer.php");
+
+?>
---
+++ setup.php
@@ -0,0 +1,324 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2007 The Cacti Group                                      |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+function plugin_map_install () {
+	api_plugin_register_hook('map', 'top_header_tabs', 'map_show_tab', 'setup.php');
+	api_plugin_register_hook('map', 'top_graph_header_tabs', 'map_show_tab', 'setup.php');
+	api_plugin_register_hook('map', 'draw_navigation_text', 'map_draw_navigation_text', 'setup.php');
+	api_plugin_register_hook('map', 'config_settings', 'map_config_settings', 'setup.php'); // personl settings info
+	api_plugin_register_hook('map', 'utilities_action', 'map_utilities_action', 'setup.php');
+	api_plugin_register_hook('map', 'utilities_list', 'map_utilities_list', 'setup.php');
+
+	api_plugin_register_realm('map', 'map.php', 'Plugin -> Map', 1);
+
+	map_setup_table();
+}
+
+function plugin_map_uninstall () {
+	// Do any extra Uninstall stuff here
+	db_execute("DROP TABLE `plugin_map_coordinate`;");
+}
+
+function plugin_map_check_config () {
+	$ret = true;
+	// Here we will check to ensure everything is configured
+	map_check_upgrade ();
+	$ret = map_check_dependencies();
+	
+	return $ret;
+}
+
+function plugin_map_upgrade () {
+	// Here we will upgrade to the newest version
+	map_check_upgrade ();
+	return false;
+}
+
+function map_version () {
+	return plugin_map_version();
+}
+
+function map_check_upgrade () {
+	global $config, $database_default;
+	include_once($config["library_path"] . "/database.php");
+	include_once($config["library_path"] . "/functions.php");
+
+	// Let's only run this check if we are on a page that actually needs the data
+	$files = array('plugins.php', 'map.php');
+	if (isset($_SERVER['PHP_SELF']) && !in_array(basename($_SERVER['PHP_SELF']), $files)) {
+		return;
+	}
+
+	$version = plugin_map_version ();
+	$current = $version['version'];
+	$old = read_config_option('plugin_map_version');
+	if ($current != $old) {
+
+		// Set the new version
+		db_execute("UPDATE plugin_config SET version='$current' WHERE directory='map'");
+		db_execute("UPDATE plugin_config SET " .
+				"version='" . $version["version"] . "', " .
+				"name='" . $version["longname"] . "', " .
+				"author='" . $version["author"] . "', " .
+				"webpage='" . $version["url"] . "' " .
+				"WHERE directory='" . $version["name"] . "' ");
+	}
+}
+
+function map_check_dependencies() {
+	global $plugins, $config;
+	if ((!in_array('settings', $plugins)) &&
+		(db_fetch_cell("SELECT directory FROM plugin_config WHERE directory='settings' AND status=1") == "")) {
+		return false;
+	}
+
+	if (!function_exists('settings_version')) {
+		if (file_exists($config['base_path'] . '/plugins/settings/setup.php')) {
+			include_once($config['base_path'] . '/plugins/settings/setup.php');
+			if (!function_exists('settings_version')) {
+				return false;
+			}
+		}
+	}
+	$v = settings_version();
+	if (!isset($v['version']) || $v['version'] < 0.3) {
+		return false;
+	}
+
+	return true;
+}
+
+function plugin_map_version () {
+	return array(
+		'name'     => 'Map',
+		'version'  => '0.1',
+		'longname' => 'Map Viewer',
+		'author'   => 'Arno Streuli',
+		'homepage' => 'http://cactiusers.org',
+		'email'    => 'astreuli@gmail.com',
+		'url'      => 'http://versions.cactiusers.org/'
+	);
+}
+
+function map_config_settings () {
+	global $tabs, $settings;
+	$tabs["misc"] = "Misc";
+
+	if (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) != 'settings.php')
+		return;
+
+	$tabs['misc'] = 'Misc';
+	$temp = array(
+		"map_general_header" => array(
+			"friendly_name" => "Map",
+			"method" => "spacer",
+			),
+		"map_tools" => array(
+			"friendly_name" => "Which maping tools",
+			"description" => "Define the mapping tools used.",
+			"method" => "drop_array",
+			'array' => array("0" => "GoogleMap", "1" => "OpenStreetMap"),
+			"default" => "20"
+			),
+		"map_api_key" => array(
+			"friendly_name" => "API Key",
+			"description" => "This is key for google map usage.",
+			"method" => "textbox",
+			"max_length" => 80,
+			"default" => ""
+			),
+		'map_log_debug' => array(
+			'friendly_name' => 'Debug Log',
+			'description' => 'Enable logging of debug messages during map file creation',
+			'method' => 'checkbox',
+			'default' => 'off'
+			),
+	);
+	
+	if (isset($settings['misc']))
+		$settings['misc'] = array_merge($settings['misc'], $temp);
+	else
+		$settings['misc']=$temp;
+
+}
+
+function map_show_tab () {
+	global $config;
+	include_once($config["library_path"] . "/database.php");
+
+	if (api_user_realm_auth('map.php')) {
+		if (!substr_count($_SERVER["REQUEST_URI"], "map.php")) {
+			print '<a href="' . $config['url_path'] . 'plugins/map/map.php"><img src="' . $config['url_path'] . 'plugins/map/images/tab_gpsmap.gif" alt="Map" align="absmiddle" border="0"></a>';
+		}else{
+			print '<a href="' . $config['url_path'] . 'plugins/map/map.php"><img src="' . $config['url_path'] . 'plugins/map/images/tab_gpsmap_down.gif" alt="Map" align="absmiddle" border="0"></a>';
+		}
+	}
+}
+
+function map_draw_navigation_text ($nav) {
+	$nav["map.php:"] = array("title" => "map", "mapping" => "", "url" => "map.php", "level" => "0");
+
+	$nav['map.php:'] = array('title' => 'map', 'mapping' => 'index.php:', 'url' => 'map.php', 'level' => '1');
+	return $nav;
+}
+
+function map_utilities_action ($action) {
+	global $config;
+	$snmpsyslocation		 = ".1.3.6.1.2.1.1.6.0"; // system location
+	include_once($config["library_path"] . '/snmp.php');
+	$maptools = read_config_option('map_tools');
+
+	if ($action == 'map_rebuild') {
+	// rebuild the map table
+		// get device list, that are not disabled and where snmp is active
+		$dbquery = db_fetch_assoc("SELECT id, hostname, snmp_community, 
+		snmp_version, snmp_username, snmp_password, snmp_port, snmp_timeout, disabled, availability_method, 
+		ping_method, ping_port, ping_timeout, ping_retries, snmp_auth_protocol, snmp_priv_passphrase, 
+		snmp_priv_protocol, snmp_context FROM host WHERE snmp_version > '0' ORDER BY id");
+
+		if (sizeof($dbquery) > 0) {
+			foreach ($dbquery as $host) {
+				// snmp_get syslocation
+				$snmp_location = cacti_snmp_get( $host['hostname'], $host['snmp_community'], $snmpsyslocation, 
+				$host['snmp_version'], $host['snmp_username'], $host['snmp_password'], 
+				$host['snmp_auth_protocol'], $host['snmp_priv_passphrase'], $host['snmp_priv_protocol'], 
+				$host['snmp_context'] ); 
+				
+				cacti_log("device: ".$host['hostname']." ".$snmp_location."\n", false, "MAP" );
+				
+				// parse google map for geocoding
+				$snmp_location = str_replace( ",", ";", $snmp_location );
+				$address = explode( ';', $snmp_location ); // Suisse;Lausanne;Chemin de Pierre-de-Plan 4;-1;Local Telecom
+				if( count($address) > 3 ) {
+					$location = $address[2]. "," .$address[1]. "," . $address[0];
+					$location = str_replace(' ', '+', $location );
+					$gpslocation = array();
+					if( $maptools == '0' ) {
+						$gpslocation = GoogleGeocode($location);
+					} else if( $maptools == '1' ) {
+						$gpslocation = OpenStreetGeocode($location);
+					}
+					cacti_log("lati: ". $gpslocation[0]." longi: ".$gpslocation[1], false, "MAP");
+				// save to new table with id and location
+				$ret = db_execute("REPLACE INTO plugin_map_coordinate (host_id, address, lat, lon) VALUES ('"
+				. sql_sanitize($host['id']) ."',\""
+				. sql_sanitize($gpslocation[2]) ."\",'"
+				. sql_sanitize($gpslocation[0]) . "', '"
+				. sql_sanitize($gpslocation[1]) . "')");
+				} else cacti_log("Snmp location error: ".$snmp_location, false, "MAP");
+			}
+		}
+
+		include_once('./include/top_header.php');
+		utilities();
+		include_once('./include/bottom_footer.php');
+	}
+	return $action;
+}
+
+function map_utilities_list () {
+	global $colors;
+
+	html_header(array("Map Plugin"), 2);
+	?>
+	<tr bgcolor="#<?php print $colors["form_alternate1"];?>">
+		<td class="textArea">
+			<a href='utilities.php?action=map_rebuild'>Rebuild mapping table</a>
+		</td>
+		<td class="textArea">
+			This will rebuild the mapping coordinate table from the device list.
+		</td>
+	</tr>
+	<?php
+}
+
+function map_setup_table () {
+	global $config, $database_default;
+	include_once($config["library_path"] . "/database.php");
+
+	$data = array();
+	$data['columns'][] = array('name' => 'host_id', 'type' => 'mediumint(8)', 'NULL' => false, 'default' => '0');
+	$data['columns'][] = array('name' => 'address', 'type' => 'varchar(150)', 'NULL' => false, 'default' => '');
+	$data['columns'][] = array('name' => 'lat', 'type' => 'float(10,6)', 'NULL' => false , 'default' => '0');
+	$data['columns'][] = array('name' => 'lon', 'type' => 'float(10,6)', 'NULL' => false, 'default' => '0');
+	$data['type'] = 'MyISAM';
+	$data['comment'] = 'Plugin map - Table of map hosts coordinate';
+	api_plugin_db_table_create('map', 'plugin_map_coordinate', $data);
+}
+
+function GoogleGeocode($address){
+	global $config;
+	$mapapikey = read_config_option('map_api_key');
+
+	//https://maps.googleapis.com/maps/api/geocode/json?address=4+chemin+pierre+de+plan,+Lausanne,+Suisse&key=AIzaSyAr0rad39hJtQLiRoPqsTstFW9u8kl6PYA
+    // url encode the address
+     
+    // google map geocode api url
+    $url = "https://maps.google.com/maps/api/geocode/json?address={$address}&key={$mapapikey}";
+ 
+    // get the json response
+    $resp_json = file_get_contents($url);
+     
+    // decode the json
+    $resp = json_decode($resp_json, true);
+ 
+    // response status will be 'OK', if able to geocode given address 
+    if($resp['status']=='OK'){
+ 
+        // get the important data
+        $lati = $resp['results'][0]['geometry']['location']['lat'];
+        $longi = $resp['results'][0]['geometry']['location']['lng'];
+        $formatted_address = $resp['results'][0]['formatted_address'];
+         
+        // verify if data is complete
+        if($lati && $longi && $formatted_address){
+         
+            // put the data in the array
+            $data_arr = array();            
+             
+            array_push(
+                $data_arr, 
+                    $lati, 
+                    $longi, 
+                    $formatted_address
+                );
+             
+            return $data_arr;
+             
+        }else{
+            return false;
+        }
+         
+    }else{
+        return false;
+    }
+}
+
+function OpenStreetGeocode($address){
+    // url encode the address
+    $address = urlencode($address);
+cacti_log("Open Street Address: ".$address."\n", false, "MAP" );
+
+}
+?>
